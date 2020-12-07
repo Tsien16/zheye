2497fdd14b678ad77637a96e5ecb02080e48e448
tsien16 <tsien16@qq.com>
2020-12-07 08:29:07 +0800

    Init Commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import ColumnList, { ColumnProps } from "@/components/ColumnList.vue";
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: "test1的专栏",
+    description: "这是test1的专栏",
+    avatar:
+      "https://pic1.zhimg.com/v2-43259f6435eb8096750388fc002bd812_1440w.jpg?source=172ae18b"
+  },
+  {
+    id: 2,
+    title: "test2的专栏",
+    description: "这是test2的专栏"
+  },
+  {
+    id: 3,
+    title: "test3的专栏",
+    description: "这是test3的专栏",
+    avatar:
+      "https://pic1.zhimg.com/v2-43259f6435eb8096750388fc002bd812_1440w.jpg?source=172ae18b"
+  },
+  {
+    id: 4,
+    title: "test4的专栏",
+    description: "这是test4的专栏",
+    avatar:
+      "https://pic1.zhimg.com/v2-43259f6435eb8096750388fc002bd812_1440w.jpg?source=172ae18b"
+  }
+];
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
+    ColumnList
+  },
+  setup() {
+    return {
+      list: testData
+    };
   }
 });
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
